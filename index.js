d81176535673c302672facc71f9d3b70c4b26085
Tim Evans <tim.c.evans@me.com>
2018-07-11 22:41:21 -0400

    make queries simpler

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const redirect = fs.readFileSync('redirect.html').toString();
 // Load current build of application from pg
 console.log('Connecting to the database...');
 client.connect().then(function () {
+  return client.query({
+    text: 'SELECT hostname, id, current_build_id, apple_developer_merchantid_domain_association FROM groups'
+  });
+}).then(function (result) {
+  let groups = result.rows;
+  let hostnames = groups.map((group) => group.hostname);
+
   const app = express();
   app.get('/health', (req, res) => res.send('❤️'));
   app.get('/torii/redirect.html', function (req, res) {
@@ -18,19 +25,18 @@ client.connect().then(function () {
   });
 
   app.get('/sitemap.xml', function (req, res) {
-    console.log(`Loading group information for ${req.headers.host}...`);
+    let group = groups.find((group) => group.hostname === req.headers.host);
+    if (group == null) {
+      console.error(`⚠️  No group found for ${req.headers.hostname}`);
+      res.send('');
+      return;
+    }
+    console.log(req.headers);
+    console.log(`ℹ️  [${group.hostname}] Creating sitemap.xml`);
 
     return client.query({
-      text: 'SELECT id FROM groups WHERE hostname=$1',
-      values: [req.headers.host]
-    }).then(() => {
-      let group = result.rows[0];
-      console.log(`Found site configuration for ${req.headers.host}.`);
-
-      return client.query({
-        text: 'SELECT slug, updated_at FROM posts WHERE group_id=$1 and published=True',
-        values: [group.id]
-      });
+      text: 'SELECT slug, updated_at FROM posts WHERE group_id=$1 and published=True',
+      values: [group.id]
     }).then((result) => {
       let urls = result.rows.map((post) => {
         // Remove precise time from the url
@@ -41,44 +47,44 @@ client.connect().then(function () {
       res.send(`<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls.join('')}</urlset>`);
     }, function (error) {
       res.send(error);
+      console.error(error);
     });
   });
 
   app.get('/.well-known/apple-developer-merchantid-domain-association', function (req, res) {
-    console.log(`Loading group information for ${req.headers.host}...`);
+    let group = groups.find((group) => group.hostname === req.headers.host);
+    if (group == null) {
+      console.error(`⚠️  No group found for ${req.headers.hostname}`);
+      res.send('');
+      return;
+    }
 
-    return client.query({
-      text: 'SELECT apple_developer_merchantid_domain_association FROM groups WHERE hostname=$1',
-      values: [req.headers.host]
-    }).then(() => {
-      let group = result.rows[0];
-      console.log(`Sending Apple Pay info for ${req.headers.host}.`);
-      res.send(group.apple_developer_merchantid_domain_association);
-    }, function (error) {
-      res.send(error);
-    });
+    console.log(req.headers);
+    console.log(`ℹ️  [${group.hostname}] Sending Apple Pay info`);
+
+    res.send(group.apple_developer_merchantid_domain_association);
   });
 
   app.get('*', function (req, res) {
-    console.log(`Loading group information for ${req.headers.host}...`);
+    let group = groups.find((group) => group.hostname === req.headers.host);
+    if (group == null) {
+      console.error(`⚠️  No group found for ${req.headers.hostname}`);
+      res.send('');
+      return;
+    }
 
-    return client.query({
-      text: 'SELECT current_build_id FROM groups WHERE hostname=$1',
-      values: [req.headers.host]
-    }).then(() => {
-      let group = result.rows[0];
-      console.log(`Loading build for ${req.headers.host}.`);
-      res.send(group.apple_developer_merchantid_domain_association);
+    console.log(req.headers);
+    console.log(`ℹ️  [${group.hostname}] Loading current build ${group.current_build_id}`);
 
-      return client.query({
-        text: 'SELECT * FROM builds WHERE id=$1',
-        values: [group.current_build_id]
-      });
+    client.query({
+      text: 'SELECT * FROM builds WHERE id=$1',
+      values: [group.current_build_id]
     }).then(function (result) {
       let build = result.rows[0];
       res.send(build.html.replace('%7B%7Bbuild.id%7D%7D', build.id));
     }, function (error) {
       res.send(error);
+      console.error(error);
     });
   });
 
