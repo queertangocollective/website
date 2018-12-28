3ed619b34631f819c2b91ae01ede208c48ff0b47
Tim Evans <tim.c.evans@me.com>
2018-12-27 21:01:28 -0500

    add sentry logging

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import { config } from 'dotenv';
 import * as knex from 'knex';
 import website from './src/app';
+import * as Sentry from '@sentry/node';
 
 config();
 
@@ -8,6 +9,8 @@ let app = website(knex({
   client: 'pg',
   connection: process.env['PGCONNECTION']
 }));
+
+Sentry.init({ dsn: process.env['SENTRY_DSN'] });
 
 let port = process.env['PORT'];
 app.listen(port, () => {
